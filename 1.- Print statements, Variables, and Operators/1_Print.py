70b6935576cbb1a7c7d946b5f460b915212e525f
GioByte10 <gvanni.bernal10@gmail.com>
2020-11-08 20:58:54 -0700

    Python

--- 1.- Print statements, Variables, and Operators/1_Print.py
+++ 1.- Print statements, Variables, and Operators/1_Print.py
@@ -18,8 +18,8 @@ x = 498
 print(x)                                           # 498
 
 print(5 == 5)                                      # True
-print(10 > 5)                                      # False
-print(10 < 5)                                      # True
+print(10 > 5)                                      # True
+print(10 < 5)                                      # False
 
 print("Concatenating" + " " + "text")              # Concatenating text
 
